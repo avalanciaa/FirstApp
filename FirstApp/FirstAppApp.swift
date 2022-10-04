5f490e9ad4ccf6e65f582419f3fa6937fbb3433c
Rayan Master <mrrayanmaster@gmail.com>
2022-10-04 19:37:45 +1100

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Rayan on 4/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
